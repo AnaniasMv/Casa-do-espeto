3bab3ab980e805f31f747e061ba29dafeede113a
MatheusV.A <ananiasmatheus22@outlook.com>
2022-02-19 10:08:52 -0300

    testando

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 const btnMobile = document.getElementById('btn-mobile');
-function toggleMenu(event){
-    if(event.type === 'touchstart') event.preventDefault;
+function toggleMenu(){
     const nav = document.getElementById('nav');
     nav.classList.toggle('active');
 
 }
 btnMobile.addEventListener('click', toggleMenu);
-btnMobile.addEventListener('touchstart', toggleMenu);
+
 
