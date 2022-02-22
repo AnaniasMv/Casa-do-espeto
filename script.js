5701b2f92c3b1d129379f3ead01785cf492213fd
MatheusV.A <ananiasmatheus22@outlook.com>
2022-02-22 08:32:31 -0300

    Removi biblioteca do bootstrap

--- script.js
+++ script.js
@@ -6,4 +6,18 @@ function toggleMenu(){
 }
 btnMobile.addEventListener('click', toggleMenu);
 
+/* GetButton.io widget */
+(function () {
+    var options = {
+        whatsapp: "+55(16) 99371-7607", // WhatsApp number
+        call_to_action: "Faça seu pedido pelo WhatsApp", // Call to action
+        position: "left", // Position may be 'right' or 'left'
+    };
+    var proto = document.location.protocol, host = "getbutton.io", url = proto + "//static." + host;
+    var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = url + '/widget-send-button/js/init.js';
+    s.onload = function () { WhWidgetSendButton.init(host, proto, options); };
+    var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(s, x);
+})();
+/*GetButton.io widget*/
+
 
